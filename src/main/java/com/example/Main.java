6b5b022afe48401336e253bb0bf24b26c111fdfe
Marcin Stachniuk <mstachniuk@gmail.com>
2017-07-28 16:59:03 +0200

    init

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,10 @@
+package com.example;
+
+import com.example.services.MyService;
+
+public class Main {
+    public static void main(String[] args) {
+        MyService myService = new MyService();
+        System.out.println("Hello World");
+    }
+}
